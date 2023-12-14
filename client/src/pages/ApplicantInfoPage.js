133cf9ef0ee570e4774b0678b4ad6185bdefb446
Yuzhuo Kang <yuzhuok@seas.upenn.edu>
2023-12-14 03:18:19 -0500

    update homepage, applicant page

--- client/src/pages/ApplicantInfoPage.js
+++ client/src/pages/ApplicantInfoPage.js
@@ -3,37 +3,42 @@
 //TASK: to implement overall %late and %payed data
 //TASK: to implement loyalty data
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 const config = require('../config.json');
 
-export default function Applicant() {
+export default function ApplicantPage() {
     const [applicantData, setApplicantData] = useState({});
     const [previousApplications, setPreviousApplications] = useState([]);
-    const [selectedPrevAppId, setSelectedPrevAppId] = useState(''); // State for selected previous application ID
-    const [inputApplicantId, setInputApplicantId] = useState('');
+    const [selectedPrevAppId, setSelectedPrevAppId] = useState('');
     const [isDataFetched, setIsDataFetched] = useState(false);
+    const { applicantId } = useParams(); // Access the applicantId from the URL
+    const navigate = useNavigate();
 
-    // Handling form submission
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setIsDataFetched(false);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Fetch applicant data
+                const applicantResponse = await fetch(`http://${config.server_host}:${config.server_port}/applicant/${applicantId}`);
+                const applicantJson = await applicantResponse.json();
+                setApplicantData(applicantJson);
 
-        try {
-            // Fetch applicant data
-            const applicantResponse = await fetch(`http://${config.server_host}:${config.server_port}/applicant/${inputApplicantId}`);
-            const applicantJson = await applicantResponse.json();
-            setApplicantData(applicantJson);
+                // Fetch previous applications data
+                const previousAppsResponse = await fetch(`http://${config.server_host}:${config.server_port}/applicant/previous_applications/${applicantId}`);
+                const previousAppsJson = await previousAppsResponse.json();
+                setPreviousApplications(previousAppsJson);
 
-            // Fetch previous applications data
-            const previousAppsResponse = await fetch(`http://${config.server_host}:${config.server_port}/applicant/previous_applications/${inputApplicantId}`);
-            const previousAppsJson = await previousAppsResponse.json();
-            setPreviousApplications(previousAppsJson);
+                setIsDataFetched(true);
+            } catch (error) {
+                console.error(error);
+                setIsDataFetched(false);
+            }
+        };
 
-            setIsDataFetched(true);
-        } catch (error) {
-            console.error(error);
+        if (applicantId) {
+            fetchData();
         }
-    };
+    }, [applicantId]);
 
     // Event handler for selecting a previous application
     const handlePrevAppSelection = (event) => {
@@ -60,31 +65,19 @@ export default function Applicant() {
         { name: "Annuity Amount", value: applicantData.amt_annuity },
     ];
 
-    const previousAppTable = previousApplications.map(app => [
-        { name: "Previous Application ID", value: app.SK_ID_PREV },
-        { name: "Processed Days Ago", value: app.processed_days_ago },
-        { name: "Status", value: app.status },
-        { name: "Reject Reason", value: app.reject_reason },
-        { name: "Percentage Late Payments", value: app.percentage_late_payments },
-        { name: "Percentage Amount Paid", value: app.percentage_amount_paid },
-    ]);
-    
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="applicantIdInput">Enter Applicant ID: </label>
-                <input
-                    id="applicantIdInput"
-                    type="text"
-                    value={inputApplicantId}
-                    onChange={(e) => setInputApplicantId(e.target.value)}
-                />
-                <button type="submit">Submit</button>
-            </form>
+            <div className="navigation-buttons">
+                <button onClick={() => navigate('/')}>Homepage</button>
+                <button onClick={() => navigate('/advanced-search')}>Advanced Search</button>
+                <button onClick={() => navigate('/analytics')}>Analytics</button>
+            </div>
+
 
             {isDataFetched && (
                 <>
-                    <h1>Applicant ID: {inputApplicantId}</h1>
+                    <h1>Applicant ID: {applicantId}</h1>
+                    {/* Render applicant data*/}
                     <table>
                         <thead>
                             <tr>
@@ -101,7 +94,7 @@ export default function Applicant() {
                             ))}
                         </tbody>
                     </table>
-
+                    
                     {/* Dropdown for selecting a previous application */}
                     <label htmlFor="prevAppSelect">Select Previous Application:</label>
                     <select id="prevAppSelect" value={selectedPrevAppId} onChange={handlePrevAppSelection}>
@@ -127,6 +120,28 @@ export default function Applicant() {
                     )}
                 </>
             )}
-        </div>
+            <div className="info-button">
+                    <button onClick={() => navigate('/info')}>More Info</button>
+                </div>
+
+                <style jsx>{`
+                    .navigation-buttons {
+                        text-align: center;
+                        margin: 10px 0;
+                    }
+                    .info-button {
+                        text-align: center;
+                        margin-top: 20px;
+                    }
+                    button {
+                        margin: 5px;
+                        padding: 10px 20px;
+                        border-radius: 5px;
+                        cursor: pointer;
+                    }
+                `}</style>
+            </div>
     );
 }
+
+
